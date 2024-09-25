991998b33c234f714990527235983d1c86f160de
Aroslav Kochkin <kochkin.2328@inbox.ru>
2024-09-25 20:23:49 +0700

    add SteckView

--- HomeWorkTreeApp/ViewController.swift
+++ HomeWorkTreeApp/ViewController.swift
@@ -12,35 +12,28 @@ class ViewController: UIViewController {
 	private let helper = Helper()
 	
 	private let nameLable = UILabel()
-	private let fullNameButton: UIButton = {
-		let button = UIButton()
-		button.setTitle("Show FullName", for: .normal)
-		button.backgroundColor = .green
-		button.frame = CGRect(x: 100, y: 150, width: 150, height: 50)
-		button.layer.cornerRadius = 20
-		button.layer.shadowColor = UIColor.black.cgColor
-		button.layer.shadowRadius = 7.0
-		button.layer.shadowOpacity = 0.7
-		return button
-	}()
+	private let fullNameButton = UIButton()
+	private let stackView = UIStackView()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		getInfoPeoples()
 		setupLable()
+		setupButton()
 		setupView()
+		setupStackView()
+		setupLayout()
 	}
 	
 	private func getInfoPeoples() {
-		helper.addUsers(helper.userReoisitory.getUsers()) // надеюсь правильно что этот клас не знает про репозиторий)) просто не знаю как еще лучше сделать внутри хелпера добавлять если только
+		helper.addUsers(helper.userReoisitory.getUsers())
 		helper.getUsers().forEach({print($0.person.fullName)})
 	}
 	
 	private func setupView() {
 		view.backgroundColor = .cyan
 		view.alpha = 0.9
-		view.addSubview(nameLable)
-		view.addSubview(fullNameButton)
+		view.addSubview(stackView)
 	}
 	
 	private func setupLable() {
@@ -48,9 +41,42 @@ class ViewController: UIViewController {
 		nameLable.text = nameText
 		nameLable.font = .systemFont(ofSize: 25, weight: .bold)
 		nameLable.textColor = .blue
-		nameLable.frame = CGRect(x: 100, y: 100, width: 150, height: 50)
+		nameLable.textAlignment = .center
 		nameLable.layer.shadowColor = UIColor.black.cgColor
 		nameLable.layer.shadowRadius = 7.0
-		nameLable.layer.shadowOpacity = 0.7	}
+		nameLable.layer.shadowOpacity = 0.7
+	}
+	
+	private func setupButton() {
+		fullNameButton.setTitle("Show FullName", for: .normal)
+		fullNameButton.backgroundColor = .green
+		fullNameButton.layer.shadowColor = UIColor.black.cgColor
+		fullNameButton.layer.shadowRadius = 7.0
+		fullNameButton.layer.shadowOpacity = 0.7
+		
+		let shadowPats = UIBezierPath(roundedRect: fullNameButton.bounds, cornerRadius: 20) // Не получается :((
+		fullNameButton.layer.shadowPath = shadowPats.cgPath
+	}
+	
+	private func setupStackView() {
+		stackView.axis = .vertical
+		stackView.distribution = .fill
+		stackView.alignment = .center
+		stackView.spacing = 10
+		
+		[nameLable, fullNameButton]
+			.forEach{stackView.addArrangedSubview($0)}
+	}
+	
+	private func setupLayout() {
+		stackView.translatesAutoresizingMaskIntoConstraints.toggle()
+		
+		NSLayoutConstraint.activate([
+			stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+			stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			stackView.widthAnchor.constraint(equalToConstant: 300),
+			stackView.heightAnchor.constraint(equalToConstant: 200)
+		])
+	}
 }
 
